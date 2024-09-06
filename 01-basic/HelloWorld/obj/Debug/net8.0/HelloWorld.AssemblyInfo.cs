b1406a55c3515eb263800859dfe317e1db05e38b
giorgos-grekis <georgrekis.github@gmail.com>
2024-09-06 19:45:49 +0300

    auto mapping

--- 01-basic/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ 01-basic/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3818a78dc28bb3b0afc9f8f57acd315ac051337b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fca5c327a612daf52e06adc3f422e2e756cde111")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
