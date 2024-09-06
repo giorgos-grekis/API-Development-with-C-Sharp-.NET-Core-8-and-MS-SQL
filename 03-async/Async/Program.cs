13d25cdeab588d5fb6e4661acb5b9c960b23526b
giorgos-grekis <georgrekis.github@gmail.com>
2024-09-06 20:19:05 +0300

    Tasks

---
+++ 03-async/Async/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Async
+{
+
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            Task fisrtName = new Task(() =>
+            {
+                Thread.Sleep(100);
+                System.Console.WriteLine("Task 1");
+            });
+            fisrtName.Start();
+            await fisrtName;
+            Console.WriteLine("After the Task was created");
+        }
+
+
+    }
+}
