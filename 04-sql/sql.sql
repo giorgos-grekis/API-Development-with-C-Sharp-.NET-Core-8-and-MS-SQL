a481106be61b890c85feb598631d92b9fbacf37d
giorgos-grekis <georgrekis.github@gmail.com>
2024-09-07 13:46:24 +0300

    more sql queries

--- 04-sql/sql.sql
+++ 04-sql/sql.sql
@@ -22,7 +22,8 @@
 --     , CPUCores INT
 --     -- BIT is a single bit 1 or 0 and translate back to true or false when we try to convert it to a Boolean in C#
 --     , HasWifi BIT  
---     , HasLite BIT
+--     , HasLite BIT,
+--    ,  ReleaseDate DATETIME 
 --     , Price DECIMAL(18, 4)
 --     , VideoCard NVARCHAR(50)
 -- }
@@ -30,3 +31,41 @@
 
 
 -- SELECT * FROM TutorialAppSchema.Computer
+
+-- -- ComputerId - you can't insert something into a table in a field that's been declared as an IDENTITY
+-- -- You can increment manual the IDENTITY with "SET IDENTITY_INSERT TutorialAppSchema.Computer ON" and after that you need to set off "SET IDENTITY_INSERT TutorialAppSchema.Computer OFF" because could cause other issues for you down the line.
+-- INSERT INTO TutorialAppSchema.Computer(
+-- [Motherboard],
+-- [CPUCores],
+-- [HasWifi],
+-- [HasLTE],
+-- [ReleaseDate],
+-- [Price],
+-- [VideoCard]
+-- ) VALUES (
+--     'Sample-Motherboard',
+--     4,
+--     1,
+--     0,
+--     '2022-01-01',
+--     1000,
+--     'Sample-Videocard'
+-- )
+
+
+-- DELETE FROM TutorialAppSchema.Computer WHERE ComputerId = 100
+
+-- UPDATE TutorialAppSchema.Computer SET CPUCores = 4 WHERE ComputerId = 100
+-- UPDATE TutorialAppSchema.Computer SET CPUCores = 4 WHERE ReleaseDate < '2017-01-01'
+-- UPDATE TutorialAppSchema.Computer SET CPUCores = NULL WHERE ReleaseDate > '2017-01-01'
+
+-- -- ISNULL return no column so we need to set an alias name with "AS" and the name we want to give "CPUCores" in this case
+-- SELECT [ComputerId],
+-- [Motherboard],
+-- ISNULL([CPUCores], 4) AS CPUCores,
+-- [HasWifi],
+-- [HasLTE],
+-- [ReleaseDate],
+-- [Price],
+-- [VideoCard] FROM TutorialAppSchema.Computer
+--     ORDER BY HasWifi DESC, ReleaseDate DESC
